93e96d0aafb1f10ccc399e19ea68878865441e53
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-06-12 21:38:59 +0200

    [deleted ] to this

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -1,11 +1,14 @@
 import Back from './components/back/Back';
 import Login from './components/login/Login'
 
+
 export default function App() {
   return (
    <>
    <Back/>
    <Login/>
+
+   
    </>
    
   )
